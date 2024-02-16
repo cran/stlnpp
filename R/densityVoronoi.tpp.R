dc9466518239dbf3e7f298ad3f0dc9973c2db5b0
Mehdi Moradi <m2.moradi@yahoo.com>
2024-02-16 17:50:06 +0000

    version 0.3.10

--- R/densityVoronoi.tpp.R
+++ R/densityVoronoi.tpp.R
@@ -1,3 +1,40 @@
+#' Intensity estimate of temporal point patterns using Voronoi-Dirichlet tessellation
+#'
+#' This function performs adaptive intensity estimation for temporal point patterns using Voronoi-Dirichlet tessellation.
+#'
+#' @usage \method{densityVoronoi}{tpp}(X, f = 1, nrep = 1, at=c("points","pixels"), dimt=128,...)
+#'
+#' @param X an object of class \code{\link{tpp}}
+#' @param f fraction (between 0 and 1 inclusive) of the data points that will be used to build a tessellation for the intensity estimate
+#' @param nrep number of independent repetitions of the randomised procedure
+#' @param at string specifying whether to compute the intensity values at a grid of pixel locations and time (at="pixels") or only at the points of x (at="points"). default is to estimate the intensity at pixels
+#' @param dimt the number of equally spaced points at which the temporal density is to be estimated. see \link[stats]{density}
+#' @param ... arguments passed to \code{\link{densityVoronoi.lpp}}
+#' 
+#' @seealso \code{\link{densityVoronoi.lpp}}, \code{\link{density.stlpp}}
+#' 
+#' @author Mehdi Moradi <m2.moradi@yahoo.com> and Ottmar Cronie
+#' 
+#' @returns 
+#' If \code{at="points"}: a vector of intensity values at the data points of X.
+#'
+#' If \code{at="pixels"}: a vector of intensity values over a grid.
+#'
+#' @details 
+#' This function computes intensity estimates for temporal point patterns using Voronoi-Dirichlet tessellation.
+#' 
+#' IF f<1, then nrep independent sub-samples of X are obtained using the function \code{\link{rthin.stlpp}}. Then for each of the obtained sub-samples, we calculate the Voronoi estimate. The final estimation is the sum of all obtained estimated intensities divided by (f*nrep). 
+#' 
+#' 
+#' @references Mateu, J., Moradi, M., & Cronie, O. (2019). Spatio-temporal point patterns on linear networks: Pseudo-separable intensity estimation. Spatial Statistics, 100400.
+#' 
+#' 
+#' @examples  
+#' X <- rpoistlpp(0.2,a=0,b=5,L=easynet)
+#' Y <- as.tpp.stlpp(X)
+#' densityVoronoi(Y)
+#' 
+#' 
 #' @export
 densityVoronoi.tpp <- function(X, f = 1, nrep = 1,
                                at=c("points","pixels"),
